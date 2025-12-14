7953ce5a1156461368993b891304d55d421649ff
IsgenderovMurad <isgenderovm985@gmail.com>
2025-12-14 23:43:12 +0400

    Vercel icon notfound update

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,23 @@
 import styles from "../Map/Map.module.css";
 
+import L from "leaflet";
+
+//Vercelde icon gorunmur deye etdim
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 import "leaflet/dist/leaflet.css";
+
+
 
 import {
   MapContainer,
